23966ee40c34e09f8ff403eb4a695a55bba26f4b
Hadi Hafizi <hadi.hafizi@gmail.com>
2017-08-03 12:22:54 -0500

    Computes Allan Factor
    Allan factor is computed according to Teich et al 1997.

---
+++ computeAllanFactor.m
@@ -0,0 +1,30 @@
+function [] = computeAllanFactor(spks, Twin, Tstrt, Tend)
+
+% Caulculates Allen Factor based on "Teich et al 1997"
+%
+% Inputs: spks: time stamps of spike times (vector)
+%         Twin: time-windows over which AF is calculated (vector)
+%         Tstrt: start time of data (spks) (scalar)
+%         Tend: end time of data (scalar)
+% 
+% Output: 
+%
+% Hadi Hafizi, Aug. 2017
+%
+%
+
+
+
+for T = 1:length(Twin)
+    
+    
+    
+    
+end
+
+
+
+
+
+
+
