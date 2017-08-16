4e8f1c83bd9e619f5add55e068380af225cf69e5
hadihafizi <mhafizi@indiana.edu>
2017-08-16 11:03:38 -0500

    update InhibID

--- InhibID.m
+++ InhibID.m
@@ -26,7 +26,7 @@ figure;loglog(ISIbin,ISInum)
 SpontFR = cell2mat(cellfun(@length,spontEvents,'uni',false))/3600;
 
 ISIstd = cell2mat(cellfun(@(x) std(diff(x)),asdf_raw(1:end-2),'uni',false))/1000;% std in seconds
-ISI = (cellfun(@(x) diff(x)/1000,asdf_raw(1:end-2),'uni',false));% std in seconds
+ISI = (cellfun(@(x) diff(x)/1000,asdf_raw(1:end-2),'uni',false));% ISI in seconds
 ISIAll = ISI{1};
 for i=2:length(ISI)
     ISIAll = [ISIAll,ISI{i}];
@@ -79,7 +79,9 @@ xlabel('std(ISI) [s]')
 
 %% Clustering - All Data
 
-ISIstd = cell(length(dataset),1); InOutDeg = cell(length(dataset),1);
+ISI = cell(length(dataset),1); ISIstd = cell(length(dataset),1); 
+ISI_All = [];
+InOutDeg = cell(length(dataset),1);
 FR = cell(length(dataset),1); SpontFR = cell(length(dataset),1);
 SpontFrac = cell(length(dataset),1); AllPars = cell(length(dataset),1);
 AllPars_Agg = [];
@@ -88,6 +90,7 @@ for i = 1:length(dataset)
     load([dataset{i},'/asdf.mat'])
     load([dataset{i},'/spontEvents.mat'])
     load([dataset{i},'/sig_te.mat'])
+    ISI{i} = cellfun(@(x) diff(x)/1000,asdf_raw(1:end-2),'uni',false);% ISI in seconds
     ISIstd{i} = cell2mat(cellfun(@(x) std(diff(x)),asdf_raw(1:end-2),'uni',false))/1000;% std in seconds
     InOutDeg{i} = (sum(sig_te,1) - sum(sig_te,2)')./ ...
         (sum(sig_te,1)+sum(sig_te,2)'); % difference b/w in- and out-deg of all neurons normalized by their sum
@@ -95,10 +98,12 @@ for i = 1:length(dataset)
     SpontFR{i} = cell2mat(cellfun(@length,spontEvents,'uni',false))/(asdf_raw{end}(2)/1000);
     SpontFrac{i} = SpontFR{i}./FR{i}; % fraction of spontaneous activity
     
+    ISI_All = [ISI_All; ISI{i}];
     AF{i}(isnan(AF{i})) = 0;
 %     AllPars{i} = [AF{i}',ISIstd{i},SpontFR{i},SpontFrac{i},InOutDeg{i}'];
-%     AllPars{i} = [AF{i}',ISIstd{i},InOutDeg{i}'];
-    AllPars{i} = [AF{i}'];
+%     AllPars{i} = [AF{i}',ISIstd{i},SpontFR{i},SpontFrac{i}];
+    AllPars{i} = [AF{i}(26,:)',ISIstd{i}];
+%     AllPars{i} = [AF{i}'];
     AllPars_Agg = [AllPars_Agg;AllPars{i}]; % Aggregate All parameters from all data sets
 end
 
@@ -110,7 +115,7 @@ figure;
 plot3(AllPars_Agg(idx==1,end-3),AllPars_Agg(idx==1,end-2),AllPars_Agg(idx==1,end),'r.','MarkerSize',20)
 hold on
 plot3(AllPars_Agg(idx==2,end-3),AllPars_Agg(idx==2,end-2),AllPars_Agg(idx==2,end),'b.','MarkerSize',20)
-plot3(AllPars_Agg(idx==3,end-3),AllPars_Agg(idx==3,end-2),AllPars_Agg(idx==3,end),'b.','MarkerSize',20)
+plot3(AllPars_Agg(idx==3,end-3),AllPars_Agg(idx==3,end-2),AllPars_Agg(idx==3,end),'k.','MarkerSize',20)
 xlabel 'std(ISI) [s]'
 ylabel 'Spontaneous FR [spk/s]'
 zlabel '(InDeg-OutDeg)/sum'
@@ -119,11 +124,32 @@ title 'Cluster Assignments'
 hold off
 grid on
 
-AllParstmp = [AF{1}(26,:)',ISIstd,SpontFrac,InOutDeg'];
-[coeff,score,latent,tsquared,explained,mu] = pca(AllParstmp);
-
+%%% plot ISI Distributions for each category
+idx1 = 1:size(AllPars_Agg,1); idx2 = 1:size(AllPars_Agg,1);
+idx1 = idx1(idx==1); idx2 = idx2(idx==2);
+ISI_Agg1 = []; ISI_Agg2 = [];
+for k = 1:length(idx1)
+    ISI_Agg1 = [ISI_Agg1, ISI_All{idx1(k)}];
+end
+for k = 1:length(idx2)
+    ISI_Agg2 = [ISI_Agg2, ISI_All{idx2(k)}];
+end
 figure;
-plot(score(:,1),score(:,2),'b.','MarkerSize',20);
+[idx1counts, idx1centers] = hist(ISI_Agg1,(0:max(ISI_Agg1))); idx1counts = idx1counts/sum(idx1counts);
+[idx2counts, idx2centers] = hist(ISI_Agg2,(0:max(ISI_Agg2))); idx2counts = idx2counts/sum(idx2counts);
+semilogx(idx1centers, idx1counts, 'r-*', 'LineWidth', 2); hold on
+semilogx(idx2centers, idx2counts, 'b-*', 'LineWidth', 2);
+legend('idx1','idx2')
+title 'ISI Distributions for K-means Categories'
+ylabel 'Probability'
+xlabel 'ISI [s]'
+
+%%%%% PCA
+% AllParstmp = [AF{1}(26,:)',ISIstd,SpontFrac,InOutDeg'];
+% [coeff,score,latent,tsquared,explained,mu] = pca(AllParstmp);
+% 
+% figure;
+% plot(score(:,1),score(:,2),'b.','MarkerSize',20);
 
 
 %% t-SNE on All Parameters
@@ -134,8 +160,25 @@ tsneSpace = tsne(AllPars_Agg);
 figure;
 plot(tsneSpace(idx==1,1),tsneSpace(idx==1,2),'b.','MarkerSize',10); hold on
 plot(tsneSpace(idx==2,1),tsneSpace(idx==2,2),'r.','MarkerSize',10)
-title 't-SNE Space'
+plot(tsneSpace(idx==3,1),tsneSpace(idx==3,2),'k.','MarkerSize',10)
+title 't-SNE Space- AF(6s), std(ISI)'
 
+%% t-SNE on ISI Distributions
+
+ISIcounts = zeros(length(ISI_All), length(0:max(cellfun(@max, ISI_All))));
+ISIcenters = zeros(length(ISI_All), length(0:max(cellfun(@max, ISI_All))));
+for k = 1:length(ISI_All)
+    [ISIcounts(k,:), ISIcenters(k,:)] = hist(ISI_All{k},(0:max(cellfun(@max, ISI_All)))); 
+    ISIcounts(k,:) = ISIcounts(k,:)/sum(ISIcounts(k,:));
+end
+
+ISItsneSpace = tsne(ISIcounts);
+
+figure;
+plot(ISItsneSpace(idx==1,1),ISItsneSpace(idx==1,2),'b.','MarkerSize',10); hold on
+plot(ISItsneSpace(idx==2,1),ISItsneSpace(idx==2,2),'r.','MarkerSize',10)
+plot(ISItsneSpace(idx==3,1),ISItsneSpace(idx==3,2),'k.','MarkerSize',10)
+title 't-SNE Space- ISI Dist'
 
 %% Correlating different parameters
 % 
