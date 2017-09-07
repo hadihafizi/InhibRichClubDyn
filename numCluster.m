a298eb322e6275c331847adcd8fd8032d0e23dd4
hadihafizi <mhafizi@indiana.edu>
2017-09-07 10:11:04 -0500

    v0.9

--- numCluster.m
+++ numCluster.m
@@ -1,16 +1,20 @@
-function numbClust = numCluster(data, varargin)
+function varargout = numCluster(data, varargin)
 
 % Compute number of clusters in a data set using Sugar and James (2003)
 % Jump method.
 %
-% Inputs: data: n-by-p (n: number of observations, p: dimension of data)
-%         maxK: max number of clusters to consider
-%         y: transformation power (distortion.^(-y), typically y = p/2)
-%         Kreps: no. of repetitions of K-means algorithm
-%         plotjumps: Whether to plot figures (True/False)
+% Inputs: data: n-by-p (n: number of observations, p: dimension of data).
+%         maxK: max number of clusters to consider.
+%         y: transformation power (distortion.^(-y), typically y = p/2).
+%         since our data are usually high-dimensional, default values are
+%         set to [1 1.5, 2].
+%         Kreps: no. of repetitions of K-means algorithm.
+%         plotjumps: Whether to plot figures (True/False).
 %         printresults: Whether to print number of clusters found
-%         (True/False)
-% Output: numbClust:
+%         (True/False).
+% Output: numbClust: no. of clusters detected with each specified value of 
+%         transformation power "y".
+%         y: corresponding values of "y".
 % 
 % Sugar, C. A., & James, G. M. (2003). Finding the number of clusters in a
 % dataset: An information-theoretic approach. Journal of the American 
@@ -72,14 +76,14 @@ dist = sumd/(n*p);
 
 transDist = zeros(length(y),length(K)+1);
 jumps = zeros(length(y),length(K));
-numbClust = zeros(length(y),1);
+numClust = zeros(length(y),1);
 for i = 1:length(y)
     % Compute the transformed distortion
     transDist(i,:) = [0, dist'.^(-y(i))];
     % Compute the jumps in transformed distortion
     jumps(i,:) = diff(transDist(i,:));
     % Compute the maximum jump
-    numbClust(i) = K(jumps(i,:) == max(jumps(i,:)));
+    numClust(i) = K(jumps(i,:) == max(jumps(i,:)));
     % Plot distortion, transformed distortion and jumps
     if (plotjumps)
         figure(103);
@@ -103,7 +107,14 @@ for i = 1:length(y)
     end
     % Report maximum jump
     if (printresults)
-        disp(['The maximum jump occurred at ',num2str(numbClust(i)), ' clusters with Y = ',num2str(y(i))]);
+        disp(['No. clusters: ',num2str(numClust(i)),' @ Y = ' ,num2str(y(i))]);
     end
 end
 
+switch nargout
+    case 1
+        varargout{1} = numClust;
+    case 2
+        varargout{1} = numClust;
+        varargout{2} = y;
+end
